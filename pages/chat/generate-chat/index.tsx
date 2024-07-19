8bf1874b794908a299df61511c7e2c17461d414c
jose-olmedo-telus <jose.olmedo@telus.com>
2024-07-18 23:52:22 -0600

    good enough layout

--- pages/chat/generate-chat/index.tsx
+++ pages/chat/generate-chat/index.tsx
@@ -1,5 +1,36 @@
 import { CoreMessage } from "ai";
 import { useState } from "react";
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+
+const CardDemo = () => {
+  return (
+    <div className="w-1/5 mx-4">
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section>
+          <Image
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+            height={160}
+            alt="Norway"
+          />
+        </Card.Section>
+
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>Norway Fjord Adventures</Text>
+          <Badge color="pink">On Sale</Badge>
+        </Group>
+
+        <Text size="sm" c="dimmed">
+          With Fjord Tours you can explore more of the magical fjord landscapes
+          with tours and activities on and around the fjords of Norway
+        </Text>
+
+        <Button color="blue" fullWidth mt="md" radius="md">
+          Book classic tour now
+        </Button>
+      </Card>
+    </div>
+  );
+};
 
 export default function Page() {
   const [input, setInput] = useState("");
@@ -88,6 +119,7 @@ export default function Page() {
   };
   return (
     <div className="flex flex-col gap-2">
+      {/*}
       <div className="flex flex-col p-2 gap-2">
         {messages.map((message, index) => (
           <div key={`${message.role}-${index}`} className="flex flex-row gap-2">
@@ -105,7 +137,13 @@ export default function Page() {
           </div>
         ))}
       </div>
-
+      */}
+      <div className="flex justify-center items-center h-screen">
+        <CardDemo />
+        <CardDemo />
+        <CardDemo />
+        <CardDemo />
+      </div>
       <div className="fixed bottom-0 p-2 w-full">
         <input
           value={input}
